0dd1529bf92ebe2a045e6133648bd4b230a70c77
Grace Kim <goooyoon@gmail.com>
2020-06-09 01:47:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,16 @@
 class Backer
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects, :project
   
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
+  
+  def back_project(project)
+    self.back_project(project)
+    @backed_projects << self
+  end
+    
   
 end
